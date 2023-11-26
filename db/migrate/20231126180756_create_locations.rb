97e7d0824beb3f5f84ec88460e393d7c06b55296
philipbroadway <me@philipbroadway.com>
2023-11-26 11:08:59 -0700

    Adding location scaffold

---
+++ db/migrate/20231126180756_create_locations.rb
@@ -0,0 +1,13 @@
+class CreateLocations < ActiveRecord::Migration[7.1]
+  def change
+    create_table :locations do |t|
+      t.text :query
+      t.integer :postalcode
+      t.decimal :lat, precision: 10, scale: 6
+      t.decimal :lng, precision: 10, scale: 6
+      t.integer :error
+
+      t.timestamps
+    end
+  end
+end
