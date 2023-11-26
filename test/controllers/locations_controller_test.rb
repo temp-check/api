4f7f8b1736642a664a98aa98fd3d30deb62e0780
philipbroadway <me@philipbroadway.com>
2023-11-26 13:22:05 -0700

    add failing tests

--- test/controllers/locations_controller_test.rb
+++ test/controllers/locations_controller_test.rb
@@ -35,4 +35,22 @@ class LocationsControllerTest < ActionDispatch::IntegrationTest
 
     assert_response :no_content
   end
+
+  test "should save invalid address with error code 404" do
+    @invalid_address = locations(:invalid_address)
+    assert_difference("Location.count") do
+      post locations_url, params: { location: { address: @invalid_address } }, as: :json
+    end
+
+    assert_equal 404, Location.last.error
+  end
+
+  test "should save invalid address with error code 401" do
+    @blank_address = locations(:blank_address)
+    assert_difference("Location.count") do
+      post locations_url, params: { location: { address: @blank_address } }, as: :json
+    end
+
+    assert_equal 401, Location.last.error
+  end
 end
