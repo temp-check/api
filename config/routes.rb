a4b5933df4a8e1bc732bcd95a0a3e1ceca87233b
philipbroadway <me@philipbroadway.com>
2023-11-26 18:05:45 -0700

    bunch of broken tests :-)

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,4 @@
 Rails.application.routes.draw do
-  resources :locations
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
@@ -8,4 +7,10 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "posts#index"
+
+  namespace :api do
+    namespace :v1 do
+      resources :locations
+    end
+  end
 end
