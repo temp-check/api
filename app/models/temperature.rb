0c449f82af9811b7177e52a2b6179e3e0af68783
philipbroadway <me@philipbroadway.com>
2023-11-27 02:42:35 -0700

    cleanup temp

--- app/models/temperature.rb
+++ app/models/temperature.rb
@@ -3,9 +3,9 @@ require 'net/http'
 class Temperature < ApplicationRecord
   belongs_to :postal_code
   validates :postal_code_id, presence: true, uniqueness: true
-  after_validation :lazily_check_forecast#, if: -> { forecast.nil? || updated_at >= 30.minutes.ago }
+  after_validation :lazy_refresh, if: -> { stale? }
 
-  def lazily_check_forecast
+  def lazy_refresh
     weather_api_key = Rails.application.credentials.api.weather
     url = URI("https://api.weatherapi.com/v1/forecast.json?key=#{weather_api_key}&q=#{postal_code.code}&days=10&aqi=no&alerts=no")
 
@@ -17,10 +17,27 @@ class Temperature < ApplicationRecord
     response = http.request(request)
     raw = response.read_body
     parsed = JSON.parse(raw)
+
     if parsed["error"]
-      self.forecast = parsed["error"]["message"]
+      self.forecast = parsed
+    
     else
       self.forecast = parsed["forecast"]["forecastday"].map { |day| { "date": day["date"], "max_f": day["day"]["maxtemp_f"], "min_f": day["day"]["mintemp_f"], "max_c": day["day"]["maxtemp_c"], "min_c": day["day"]["mintemp_c"], "icon": day["day"]["condition"]["icon"].gsub("//","https://") } }
     end
   end
+
+  def api_error
+    if self.forecast.is_a?(Hash) && !self.forecast["error"].nil?
+      self.forecast["error"]
+    else
+      nil
+    end
+  end
+
+  private
+
+  def stale?
+    updated_at && updated_at < 30.minutes.ago || !api_error.nil?
+  end
+
 end
