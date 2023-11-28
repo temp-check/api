72563cda4e48a7649d1c00b1ad22b5d3ae20804f
philipbroadway <me@philipbroadway.com>
2023-11-28 08:59:08 -0700

    fix tests

--- app/controllers/api/v1/address_controller.rb
+++ app/controllers/api/v1/address_controller.rb
@@ -3,13 +3,12 @@ module Api
     class AddressController < BaseController
       def search
         @location = Location.find_or_create_by(address: params[:q])
-
         if @location.errors.any? || !@location.geocode_error.nil?
           errors = @location.errors.full_messages + [@location.geocode_error]
           render errors: errors, status: :unprocessable_entity
         else
           render json: @location.as_json(include: { postal_code: { include: :temperature } }), status: :ok
-          @location.postal_code.temperature.update_attribute(:cached, true)
+          @location&.postal_code&.temperature&.update_attribute(:cached, true)
         end
       end
     end
